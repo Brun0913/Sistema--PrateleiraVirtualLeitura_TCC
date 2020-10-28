6f6bad04d17ca4870e54840f5bc07614e0193cb8
DaviRocha14Sp <63364248+DaviRocha14Sp@users.noreply.github.com>
2020-10-28 20:45:05 -0300

    tela Finalizar Compra

---
+++ frontend/bookmanager/src/Pages/menucliente/FinalizarCompra/finalizar.js
@@ -0,0 +1,53 @@
+import React from'react';
+import './finalizar.css';
+import {Link} from 'react-router-dom';
+
+import codigo from'./codigo.png'
+
+
+
+function FinalizarCompra(){
+    return(
+        <div className='nome'>
+            
+                <div className='drag'>
+                    <h1>Parabéns!Compra concluída!</h1>
+                </div>
+
+                <div className='jg'>
+                    <h3>Você deseja baixar o livro ?</h3>
+                </div>
+
+                <div className='codigo'>
+                <img src={codigo}/>
+                </div>
+
+            <div className='escolha'>
+                
+                <div className="sim">
+                    <button variant="gray" size="lg" block>
+                        <h4>Sim</h4>  
+                    </button>
+                </div>
+                
+                <div className="não">
+                    <button variant="gray" size="lg" block>
+                        <h4>Não</h4>
+                    </button>
+                </div>
+            </div>
+
+                <div className="voltar">
+                    <button variant="gray" size="lg" block>
+                        <a href='/menucliente'> Voltar </a>
+                    </button>
+                </div>
+        </div>
+
+        
+
+    )
+}
+
+
+export default FinalizarCompra;
