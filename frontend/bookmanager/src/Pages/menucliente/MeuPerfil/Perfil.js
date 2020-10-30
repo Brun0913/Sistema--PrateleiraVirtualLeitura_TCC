d957ad543a0a97ea4ef7618ef3d0ed9a12da132b
DaviRocha14Sp <63364248+DaviRocha14Sp@users.noreply.github.com>
2020-10-30 19:10:46 -0300

    subindo Historico de compras, Meu perfil, Gerenciar Finanças

--- frontend/bookmanager/src/Pages/menucliente/MeuPerfil/Perfil.js
+++ frontend/bookmanager/src/Pages/menucliente/MeuPerfil/Perfil.js
@@ -5,60 +5,63 @@ import { Link } from "react-router-dom";
 
 function MeuPerfil(){
     return(
-    <div className='mae'>
-        <div className='perfi1'>
-            <h1>Meu Perfil</h1>
-        </div>
+    <div className='um'>
 
-        <div className='pai'>
-
-
-            <div className='pessoa'>
-                <h3>Nome</h3>
-            </div>
-            
-            <div className='niver'>
-                <h3>Data de nascimento</h3>
+            <div className='perfi1'>
+                <h1>Meu Perfil</h1>
             </div>
 
-            <div className='cpf'>
-                <h3>CPF</h3>
-            </div>
+        <div className='dois'>
 
-            <div className='rg'>
-                <h3>R.g</h3>
-            </div>
-
-            <div className='cartao'>
-                <h3>Número do cartão</h3>
-            </div>
-
-            <div className='CDC'>
-                <h3>Código de segurança (cartão) </h3>
-            </div>
-
-            <div className='vencimento'>
-                <h3>Data de vencimento do cartão</h3>
-            </div>
-
-            <div className='rua'>
-                <h3>Endereço</h3>
-            </div>
+            <div className='pai'>
 
 
-            </div>
-            
-            
+                <div className='pessoa'>
+                    <h3>Nome</h3>
+                </div>
+
+                <div className='niver'>
+                    <h3>Data de nascimento</h3>
+                </div>
+
+                <div className='cpf'>
+                    <h3>CPF</h3>
+                </div>
+
+                <div className='rg'>
+                    <h3>R.g</h3>
+                </div>
+
+                <div className='cartao'>
+                    <h3>Número do cartão</h3>
+                </div>
+
+                <div className='CDC'>
+                    <h3>Código de segurança (cartão) </h3>
+                </div>
+
+                <div className='vencimento'>
+                    <h3>Data de vencimento do cartão</h3>
+                </div>
+
+                <div className='rua'>
+                    <h3>Endereço</h3>
+                </div>
 
 
-            <div className="voltar">
-                <Link to="/menucliente">
-                    <button variant="gray" size="lg" block>
-                        Voltar
-                    </button> 
-                </Link>
-            </div>
+                </div>
 
+
+
+
+                <div className="voltar">
+                    <Link to="/menucliente">
+                        <button variant="gray" size="lg" block>
+                            Voltar
+                        </button> 
+                    </Link>
+                </div>
+            </div>  
         </div>
     )
 }
