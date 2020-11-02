9b495d93232da90d0279389189ed93f76561a8da
Brun0913 <66215701+Brun0913@users.noreply.github.com>
2020-11-02 00:58:38 -0300

    arrumando

--- frontend/bookmanager/src/Pages/menugerente/menugerente.js
+++ frontend/bookmanager/src/Pages/menugerente/menugerente.js
@@ -4,10 +4,7 @@ import './menugerente.css';
 import { Link } from "react-router-dom";
 
 function MenuGerente(props){
-<<<<<<< HEAD
-=======
 
->>>>>>> 340c7261cef78154604c5b35e4b714563d509949
     return(
       <body>
         <div className="prigerente">
@@ -48,10 +45,4 @@ function MenuGerente(props){
     )
 }
 
-<<<<<<< HEAD
-
 export default MenuGerente;
-
-=======
-export default MenuGerente;
->>>>>>> 340c7261cef78154604c5b35e4b714563d509949
