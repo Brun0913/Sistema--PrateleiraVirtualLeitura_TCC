349a3c2ba144a914a189d48926998f69bdf3b321
Brun0913 <66215701+Brun0913@users.noreply.github.com>
2020-11-03 02:29:05 -0300

    arrumado

--- frontend/bookmanager/src/Pages/Services/FuncoesCliente.js
+++ frontend/bookmanager/src/Pages/Services/FuncoesCliente.js
@@ -15,20 +15,11 @@ export default class FuncoesCliente{
         return x.data;
     }
     async FazerCompra(idlivro,idcliente){
-<<<<<<< HEAD
         const x = api.post("/fazercompra/"+ idlivro + "/" + idcliente)
         return x.data;
     }
     async Perfil(id){
         const x = await api.get("/perfil/" + id);
         return x.data;
-=======
-        const x = api.post("fazercompra/"+ idlivro + "/" + idcliente)
-        return x.data;;
-    }
-    async Perfil(id){
-        const x = await api.get("perfil/" + id);
-        return x.data
->>>>>>> abb956a0a25a03dc39c5027e324333df5edbc4b9
     }
 }
